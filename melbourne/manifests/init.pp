3310dd405d89ba8072fe953c1a07a79994b4e763
trustarun <trustarun@yahoo.co.in>
2014-08-29 18:07:02 +0530

    melbourne module commited

---
+++ melbourne/manifests/init.pp
@@ -0,0 +1,33 @@
+class melbourne{
+
+    class{ 'thirdpillar::create_users':
+                before => Class['thirdpillar::build_ruby'],
+        }
+    class{ 'thirdpillar::build_ruby':
+                before => Class['thirdpillar::set_git'],
+        }
+    class{ 'thirdpillar::set_git':
+                before => Class['thirdpillar::set_rvm'],
+        }
+    class{ 'thirdpillar::set_rvm':
+                before => Class['thirdpillar::build_mysql'],
+        }
+    class{ 'thirdpillar::build_mysql':
+                before => Class['thirdpillar::build_passenger'],
+        }
+    class{ 'thirdpillar::build_passenger':
+                before => Class['thirdpillar::server'],
+        }
+    class{ 'thirdpillar::server':
+                before => Class['thirdpillar::server_addon'],
+        }
+    class{ 'thirdpillar::server_addon':
+                before => Class['thirdpillar::gem_dev_packages'],
+        }
+    class{ 'thirdpillar::gem_dev_packages':
+		before => Class['thirdpillar::commands'],
+        }
+    class{ 'thirdpillar::commands':
+
+        }
+  }
